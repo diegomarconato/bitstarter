5f7ea506d7c5c30c26d95ce7bd5359d913149838
Diego Marconato <diego.marconato@gmail.com>
2013-07-13 17:37:29 +0200

    Update web.js

--- web.js
+++ web.js
@@ -1,12 +1,23 @@
+fs = require('fs')
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+ fs.readFile('index.html', 'utf8', function(err, data) {
+
+                if (err){
+                response.write('Could not find or open file for reading\n');
+                }
+                else{
+                var buffer = new Buffer(data, 'utf-8');
+                response.send(buffer);
+                }
+        });
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
+
